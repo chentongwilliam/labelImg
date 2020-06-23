421f54567a6a74cc348231050d3df6fa6753acc0
chentongwilliam <33293588+chentongwilliam@users.noreply.github.com>
2020-06-23 21:21:12 +0200

    replace PyQt5 with PySide2

--- libs/zoomWidget.py
+++ libs/zoomWidget.py
@@ -1,10 +1,6 @@
-try:
-    from PyQt5.QtGui import *
-    from PyQt5.QtCore import *
-    from PyQt5.QtWidgets import *
-except ImportError:
-    from PyQt4.QtGui import *
-    from PyQt4.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtCore import *
+from PySide2.QtWidgets import *
 
 
 class ZoomWidget(QSpinBox):
