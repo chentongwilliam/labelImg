328557981ddf446c2a98987c4e88b59d05a7fc9d
chentongwilliam <33293588+chentongwilliam@users.noreply.github.com>
2020-06-23 21:03:15 +0200

    replace PyQt5 with PySide2
    QVariant = str

--- labelImg.py
+++ labelImg.py
@@ -11,20 +11,11 @@ import subprocess
 from functools import partial
 from collections import defaultdict
 
-try:
-    from PyQt5.QtGui import *
-    from PyQt5.QtCore import *
-    from PyQt5.QtWidgets import *
-except ImportError:
-    # needed for py3+qt4
-    # Ref:
-    # http://pyqt.sourceforge.net/Docs/PyQt4/incompatible_apis.html
-    # http://stackoverflow.com/questions/21217399/pyqt4-qtcore-qvariant-object-instead-of-a-string
-    if sys.version_info.major >= 3:
-        import sip
-        sip.setapi('QVariant', 2)
-    from PyQt4.QtGui import *
-    from PyQt4.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtCore import *
+from PySide2.QtWidgets import *
+
+QVariant = str
 
 from combobox import ComboBox
 from libs.resources import *
