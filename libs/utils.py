3c667f2fcc7abad129913159d2246177bd97e7b7
chentongwilliam <33293588+chentongwilliam@users.noreply.github.com>
2020-06-23 21:20:41 +0200

    replace PyQt5 with PySide2

--- libs/utils.py
+++ libs/utils.py
@@ -4,13 +4,9 @@ import hashlib
 import re
 import sys
 
-try:
-    from PyQt5.QtGui import *
-    from PyQt5.QtCore import *
-    from PyQt5.QtWidgets import *
-except ImportError:
-    from PyQt4.QtGui import *
-    from PyQt4.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtCore import *
+from PySide2.QtWidgets import *
 
 
 def newIcon(icon):
